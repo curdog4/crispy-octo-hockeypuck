28475957d272948ffc62df48e41da9a1c1664a56
David Curdie <curdog4@curdie.us>
2015-11-10 21:48:40 -0800

    adding argparse and ConfigParser to parameterize configuration

--- weather_conditions.py
+++ weather_conditions.py
@@ -5,17 +5,91 @@ import urllib2
 import time
 import logging
 import json
+import ConfigParser
 from string import Template
+from argparse import ArgumentParser
+
+cfgParser = ConfigParser.ConfigParser()
+
+parser = ArgumentParser(description='Communicate with the WeatherUnderground API')
+parser.add_argument("-v","--verbose",dest="verbose",action="store_true",default=False,
+                    help="output verbose log messages")
+parser.add_argument("-d","--debug",dest="debug",action="store_true",default=False,
+                    help="output debug log messages")
+parser.add_argument("-c","--config",dest="config",default=None,
+                    metavar="CONFIG",help="use configuration from CONFIG file (ConfigParser compatible)")
+parser.add_argument("-k","--api-key",dest="apikey",default=None,
+                    metavar="APIKEY",help="use APIKEY when authenticating with the WeatherUnderground API")
+parser.add_argument("-s","--service-name",dest="servicenames",default=[],action="append",
+                    metavar="SERVICE",help="use SERVICE service from the WeatherUnderground API")
+parser.add_argument("-l","--location",dest="location",default=None,
+                    metavar="LOCATION",help="query WeatherUnderground for location LOCATION (US City/State | US Zipcode | Country/City | Latitude,Longitude | Airport Code | PWS ID)")
+
+options = parser.parse_args()
+
+if options.debug == True:
+    logLevel = logging.DEBUG
+elif options.verbose == True:
+    logLevel = logging.INFO
+else:
+    logLevel = logging.WARN
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+ch.setLevel(logLevel)
 
 fh = logging.Formatter("[%(asctime)s] %(levelname)s: %(filename)s:%(funcName)s:%(lineno)d - %(message)s")
 ch.setFormatter(fh)
 logger.addHandler(ch)
+
+(fileConfig, apikey, servicenames, location) = (None, None, [], None)
+if options.config is not None:
+    try:
+        fileConfig = cfgParser.read(options.config)
+    except Exception as e:
+        logger.error("Problem reading configuration from '%s'. Aborting.", options.config)
+        sys.exit(1)
+if options.apikey is None:
+    if fileConfig is not None:
+        try:
+            apikey = cfgParser.get('default','apikey')
+        except Exception as e:
+            logger.error("Unable to find key 'apikey' in section 'default' from config file '%s': %s",
+                         options.config, e)
+            sys.exit(1)
+    else:
+        logger.error("No API key provided. Unable to continue.")
+        sys.exit(1)
+else:
+    apikey = options.apikey
+if len(options.servicenames) == 0:
+    if fileConfig is not None:
+        try:
+            servicenames = cfgParser.get('default','servicenames').split()
+        except Exception as e:
+            logger.error("Unable to find key 'servicenames' in section 'default' from config file '%s': %s",
+                         options.config, e)
+            sys.exit(1)
+    else:
+        logger.error("No service names provided to query. Unable to continue.")
+        sys.exit(1)
+else:
+    servicenames = options.servicenames
+if options.location is None:
+    if fileConfig is not None:
+        try:
+            location = cfgParser.get('default','location')
+        except Exception as e:
+            logger.error("Unable to find key 'location' in section 'default' from config file '%s': %s",
+                         options.config, e)
+            sys.exit(1)
+    else:
+        logger.error("No location provided. Unable to continue.")
+        sys.exit(1)
+else:
+    location = options.location
 
 '''
 Using the weath API made available by Weather Underground:
@@ -23,13 +97,12 @@ Using the weath API made available by Weather Underground:
 '''
 
 config = {
-    'apikey': '35dd69aac64cd95c',
-    'servicename': 'geolookup/conditions',
-    'countryname': 'Indonesia',
-    'cityname': 'Jakarta'
+    'apikey': apikey,
+    'feature': '/'.join(servicenames),
+    'query': location
 }
 
-tmpl = Template('http://api.wunderground.com/api/${apikey}/${servicename}/q/${countryname}/${cityname}.json')
+tmpl = Template('http://api.wunderground.com/api/${apikey}/${feature}/q/${query}.json')
 
 url = tmpl.substitute(config)
 logger.info("Requesting URL '%s'", url)
